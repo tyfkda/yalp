2056ca9b428a2f015a5df2ae32617863580ef748
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2013-11-09 15:08:26 +0900

    Add readString method

--- test/macp_test.cc
+++ test/macp_test.cc
@@ -3,7 +3,20 @@
 
 using namespace macp;
 
-TEST(MacpTest, Create) {
-  State* state = State::create();
-  delete state;
+class MacpTest : public ::testing::Test {
+protected:
+  virtual void SetUp() override {
+    state_ = State::create();
+  }
+
+  virtual void TearDown() override {
+    delete state_;
+  }
+
+  State* state_;
+};
+
+TEST_F(MacpTest, Read) {
+  Svalue s = state_->readString("123");
+  ASSERT_EQ(123, s);
 }
