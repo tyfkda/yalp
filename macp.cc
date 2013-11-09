723d36e2dcc6cdb1bc3a3a1246a7e4e55e30b667
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2013-11-09 16:11:23 +0900

    Make Svalue class

--- macp.cc
+++ macp.cc
@@ -7,7 +7,37 @@
 #include <stdlib.h>
 
 namespace macp {
+//=============================================================================
+Svalue::Svalue(Sfixnum i)
+  : v_(reinterpret_cast<Sfixnum>(i) | TAG_FIXNUM) {}
 
+Svalue::Svalue(class Sobject* object)
+  : v_(reinterpret_cast<Sfixnum>(object) | TAG_OBJECT) {}
+
+Type Svalue::getType() const {
+  switch (v_ & TAG_MASK) {
+  default:
+    assert(false);
+    return TT_UNKNOWN;
+  case TAG_FIXNUM:
+    return TT_FIXNUM;
+  case TAG_OBJECT:
+    return toObject()->getType();
+  }
+}
+
+Sfixnum Svalue::toFixnum() const {
+  assert((v_ & TAG_MASK) == TAG_FIXNUM);
+  assert(TAG_FIXNUM == 0);
+  return reinterpret_cast<Sfixnum>(v_);
+}
+
+Sobject* Svalue::toObject() const {
+  assert((v_ & TAG_MASK) == TAG_OBJECT);
+  return reinterpret_cast<Sobject*>(v_ & ~TAG_OBJECT);
+}
+
+//=============================================================================
 State* State::create() {
   return new State;
 }
@@ -22,40 +52,9 @@ Svalue State::readString(const char* str) {
   return fixnumValue(atoi(str));
 }
 
-Type State::getType(Svalue v) const {
-  switch (v & TAG_MASK) {
-  default:
-    assert(false);
-    break;
-  case TAG_FIXNUM:
-    return TT_FIXNUM;
-  case TAG_OBJECT:
-    return toObject(v)->getType();
-  }
-}
-
-Svalue State::fixnumValue(Sfixnum i) {
-  return reinterpret_cast<Svalue>(i) | TAG_FIXNUM;
-}
-
-Sfixnum State::toFixnum(Svalue v) {
-  assert((v & TAG_MASK) == TAG_FIXNUM);
-  assert(TAG_FIXNUM == 0);
-  return reinterpret_cast<Sfixnum>(v);
-}
-
 Svalue State::cons(Svalue a, Svalue d) {
   Scell* cell = new Scell(a, d);
-  return objectValue(cell);
-}
-
-Svalue State::objectValue(Sobject* o) {
-  return reinterpret_cast<Svalue>(o) | TAG_OBJECT;
-}
-
-Sobject* State::toObject(Svalue v) const {
-  assert((v & TAG_MASK) == TAG_OBJECT);
-  return reinterpret_cast<Sobject*>(v & ~TAG_OBJECT);
+  return Svalue(cell);
 }
 
 
