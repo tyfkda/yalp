c9c229b090a84d79fa629ca71f423066b25738e2
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2013-11-21 13:24:53 +0900

    Rename yalp.c to state.c

--- src/yalp.cc
+++ src/yalp.cc
@@ -1,193 +1,101 @@
-//=============================================================================
-/// YALP - Yet Another List Processor.
-//=============================================================================
-
 #include "yalp.hh"
-#include "yalp/object.hh"
-#include "symbol_manager.hh"
-#include "vm.hh"
-#include <assert.h>
+#include "yalp/mem.hh"
+#include "yalp/read.hh"
+#include <fstream>
 #include <iostream>
 
-namespace yalp {
+using namespace std;
+using namespace yalp;
 
-const Sfixnum TAG_SHIFT = 2;
-const Sfixnum TAG_MASK = (1 << TAG_SHIFT) - 1;
-const Sfixnum TAG_FIXNUM = 0;
-const Sfixnum TAG_OBJECT = 1;
+static int nnew, ndelete;
 
-//=============================================================================
-Svalue::Svalue() : v_(TAG_OBJECT) {
-  // Initialized to illegal value.
+void* operator new(size_t size) {
+  ++nnew;
+  return malloc(size);
 }
 
-Svalue::Svalue(Sfixnum i)
-  : v_(reinterpret_cast<Sfixnum>(i << TAG_SHIFT) | TAG_FIXNUM) {}
+void operator delete(void* p) noexcept {
+  if (p == NULL)
+    cout << "delete called for NULL" << endl;
+  ++ndelete;
+  free(p);
+}
 
-Svalue::Svalue(class Sobject* object)
-  : v_(reinterpret_cast<Sfixnum>(object) | TAG_OBJECT) {}
+void* operator new[](size_t size) {
+  ++nnew;
+  return malloc(size);
+}
 
-Type Svalue::getType() const {
-  switch (v_ & TAG_MASK) {
-  default:
-    assert(false);
-    return TT_UNKNOWN;
-  case TAG_FIXNUM:
-    return TT_FIXNUM;
-  case TAG_OBJECT:
-    return toObject()->getType();
+void operator delete[](void* p) noexcept {
+  if (p == NULL)
+    cout << "delete called for NULL" << endl;
+  ++ndelete;
+  free(p);
+}
+
+class MyAllocator : public Allocator {
+public:
+  int nalloc, nfree;
+  MyAllocator() : nalloc(0), nfree(0) {}
+
+  virtual void* alloc(size_t size) override {
+    ++nalloc;
+    return ::malloc(size);
   }
-}
-
-std::ostream& operator<<(std::ostream& o, Svalue v) {
-  switch (v.v_ & TAG_MASK) {
-  default:
-    assert(false);
-    return o;
-  case TAG_FIXNUM:
-    o << v.toFixnum();
-    return o;
-  case TAG_OBJECT:
-    return o << *v.toObject();
+  virtual void* realloc(void* p, size_t size) override {
+    if (p == NULL)
+      ++nalloc;
+    return ::realloc(p, size);
   }
+  virtual void free(void* p) override {
+    ::free(p);
+    ++nfree;
+  }
+};
+
+void runBinary(State* state, std::istream& istrm) {
+  Reader reader(state, istrm);
+  
+  Svalue bin;
+  ReadError err;
+  while ((err = reader.read(&bin)) == READ_SUCCESS) {
+    state->runBinary(bin);
+  }
+  if (err != END_OF_FILE)
+    cerr << "Read error: " << err << endl;
 }
 
-Sfixnum Svalue::toFixnum() const {
-  assert((v_ & TAG_MASK) == TAG_FIXNUM);
-  assert(TAG_FIXNUM == 0);
-  return reinterpret_cast<Sfixnum>(v_ >> TAG_SHIFT);
-}
+int main(int argc, char* argv[]) {
+  MyAllocator myAllocator;
+  State* state = State::create(&myAllocator);
 
-Sobject* Svalue::toObject() const {
-  assert((v_ & TAG_MASK) == TAG_OBJECT);
-  return reinterpret_cast<Sobject*>(v_ & ~TAG_OBJECT);
-}
-
-bool Svalue::equal(Svalue target) const {
-  switch (v_ & TAG_MASK) {
-  default:
-    assert(false);
-    return false;
-  case TAG_FIXNUM:
-    return eq(target);
-  case TAG_OBJECT:
-    {
-      Type t1 = getType();
-      Type t2 = target.getType();
-      if (t1 != t2)
-        return false;
-
-      return toObject()->equal(target.toObject());
+  bool bOutMemResult = false;
+  int ii;
+  for (ii = 1; ii < argc; ++ii) {
+    char* arg = argv[ii];
+    if (arg[0] != '-')
+      break;
+    switch (arg[1]) {
+    case 'm':
+      bOutMemResult = true;
+      break;
+    default:
+      cerr << "Unknown option: " << arg << endl;
     }
   }
-}
 
-//=============================================================================
-State* State::create() {
-  return create(Allocator::getDefaultAllocator());
-}
-
-State* State::create(Allocator* allocator) {
-  void* memory = allocator->alloc(sizeof(State));
-  return new(memory) State(allocator);
-}
-
-void State::release() {
-  Allocator* allocator = allocator_;
-  this->~State();
-  allocator->free(this);
-}
-
-State::State(Allocator* allocator)
-  : allocator_(allocator)
-  , symbolManager_(SymbolManager::create(allocator))
-  , nil_(symbolManager_->intern("nil"))
-  , t_(symbolManager_->intern("t"))
-  , quote_(symbolManager_->intern("quote"))
-  , vm_(NULL) {
-  vm_ = Vm::create(this);
-}
-
-State::~State() {
-  symbolManager_->release();
-}
-
-Svalue State::runBinary(Svalue code) {
-  return vm_->run(code);
-}
-
-Svalue State::intern(const char* name) {
-  return Svalue(symbolManager_->intern(name));
-}
-
-Svalue State::gensym() {
-  return Svalue(symbolManager_->gensym());
-}
-
-Svalue State::cons(Svalue a, Svalue d) {
-  void* memory = allocator_->alloc(sizeof(Cell));
-  Cell* cell = new(memory) Cell(a, d);
-  return Svalue(cell);
-}
-
-Svalue State::quote(Svalue x) {
-  return list(this, quote_, x);
-}
-
-Svalue State::stringValue(const char* string) {
-  int len = strlen(string);
-  void* stringBuffer = allocator_->alloc(sizeof(char) * (len + 1));
-  char* copiedString = new(stringBuffer) char[len + 1];
-  strcpy(copiedString, string);
-
-  void* memory = allocator_->alloc(sizeof(String));
-  String* s = new(memory) String(copiedString);
-  return Svalue(s);
-}
-
-int State::getArgNum() const {
-  return vm_->getArgNum();
-}
-
-Svalue State::getArg(int index) const {
-  return vm_->getArg(index);
-}
-
-void State::runtimeError(const char* msg) {
-  std::cerr << msg << std::endl;
-  exit(1);
-}
-
-//=============================================================================
-
-Svalue list(State* state, Svalue v1) {
-  return state->cons(v1, state->nil());
-}
-
-Svalue list(State* state, Svalue v1, Svalue v2) {
-  return state->cons(v1, state->cons(v2, state->nil()));
-}
-
-Svalue list(State* state, Svalue v1, Svalue v2, Svalue v3) {
-  return state->cons(v1, state->cons(v2, state->cons(v3, state->nil())));
-}
-
-Svalue nreverse(State* state, Svalue v) {
-  if (v.getType() != TT_CELL)
-    return v;
-
-  Svalue tail = state->nil();
-  for (;;) {
-    Cell* cell = static_cast<Cell*>(v.toObject());
-    Svalue d = cell->cdr();
-    cell->rplacd(tail);
-    if (d.getType() != TT_CELL)
-      return v;
-    tail = v;
-    v = d;
+  if (ii >= argc) {
+    runBinary(state, cin);
+  } else {
+    std::ifstream strm(argv[ii]);
+    runBinary(state, strm);
   }
+
+  state->release();
+
+  if (bOutMemResult) {
+    printf("#new: %d, #delete: %d\n", nnew, ndelete);
+    printf("#alloc: %d, #free: %d\n", myAllocator.nalloc, myAllocator.nfree);
+  }
+  return 0;
 }
-
-//=============================================================================
-
-}  // namespace yalp
