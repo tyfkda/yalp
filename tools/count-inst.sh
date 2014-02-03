d076a39226514a22a76005a36297e6d3ec79e21d
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2014-02-03 19:41:13 +0900

    Fix tools to handle encoded opcode

--- tools/count-inst.sh
+++ tools/count-inst.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-./yalp -L ./boot.bin tools/stat.yl ./boot.bin \
+./yalp -L boot.bin tools/stat.yl boot.bin \
+    | ./yalp -L boot.bin tools/decode.yl \
     | sort | uniq -c | awk '{if ($1 > 1) { print }}' | sort -nr
