cc462f566ba20df854aea5d1742ce9dd4483c383
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2014-02-10 11:03:20 +0900

    Optimize compiled result

--- tools/build-compiler.sh
+++ tools/build-compiler.sh
@@ -4,5 +4,6 @@
 set -e
 
 ./yalp -C compiler/boot.yl compiler/backquote.yl compiler/util.yl \
-          compiler/setf.yl compiler/node.yl compiler/compiler.yl \
-  | ./yalp tools/optimize.yl
+          compiler/setf.yl compiler/node.yl \
+          compiler/match.yl compiler/code-walker.yl compiler/optimize.yl \
+          compiler/compiler.yl
