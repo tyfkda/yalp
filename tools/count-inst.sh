17c20330334f9572a88c3afc64342764c4f03806
Tsuyoshi Fukuda <tyfkda+gh@gmail.com>
2014-02-10 21:13:07 +0900

    Fix tools to work

--- tools/count-inst.sh
+++ tools/count-inst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./yalp -L boot.bin tools/stat.yl boot.bin \
-    | ./yalp -L boot.bin tools/decode.yl \
+./yalp tools/stat.yl boot.bin \
+    | ./yalp tools/decode.yl \
     | sort | uniq -c | awk '{if ($1 > 1) { print }}' | sort -nr
